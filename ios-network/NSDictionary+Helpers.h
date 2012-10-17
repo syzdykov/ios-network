5546aa5d3de1f9a6fc5d019ef5c6d3a9ceed4a54
VoidLess <mail@voidless.ru>
2012-10-17 15:25:48 +0400

    add extended NSDictionary+Helpers.h

--- ios-network/NSDictionary+Helpers.h
+++ ios-network/NSDictionary+Helpers.h
@@ -2,24 +2,46 @@
 //  NSDictionary+Helpers.h
 //  Studio Mobile
 //
-//  Created by Developer on 2/16/12.
-//  Copyright (c) 2012 Studio Mobile. All rights reserved.
+//  Created by Sergey Martynov on 24.02.12.
+//  Copyright (c) 2012 StudioMobile. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
 @interface NSDictionary (Helpers)
 
-- (NSURL*)urlForKey:(id)key;
+- (id)objectAtIndex:(NSUInteger)index;
 
-- (id)objectNotNullForKey:(id)key;
+- (NSInteger)integerForKey:(id)key;
 
-- (NSDate *)dateForKey:(id)key;
+- (NSInteger)integerForKey:(id)key default:(NSInteger)def;
 
-- (NSNumber *)boolForKey:(id)key;
+- (NSUInteger)unsignedIntegerForKey:(id)key;
 
-- (NSNumber *)intForKey:(id)key;
+- (NSUInteger)unsignedIntegerForKey:(id)key default:(NSUInteger)def;
 
-- (NSNumber *)floatForKey:(id)key;
+- (BOOL)boolForKey:(id)key;
+
+- (float)floatForKey:(id)key;
+
+- (double)doubleForKey:(id)key;
+
+- (id)nonNullObjectForKey:(id)key;
+
+- (NSDate*)dateForKey:(id)key formatter:(NSDateFormatter*)formatter;
+
+- (NSString*)stringForKey:(id)key;
+
+- (NSDictionary*)dictionaryForKey:(id)key;
+
+- (NSArray*)arrayForKey:(id)key;
+
+- (NSNumber*)numberForKey:(id)key;
+
+@end
+
+@interface NSMutableDictionary (Helpers)
+
+- (void)replaceObjectAtIndex:(NSUInteger)index withObject:(id)obj;
 
 @end
--- ios-network/NSDictionary+Helpers.m
+++ ios-network/NSDictionary+Helpers.m
@@ -2,56 +2,100 @@
 //  NSDictionary+Helpers.m
 //  Studio Mobile
 //
-//  Created by Developer on 2/16/12.
-//  Copyright (c) 2012 Studio Mobile. All rights reserved.
+//  Created by Sergey Martynov on 24.02.12.
+//  Copyright (c) 2012 StudioMobile. All rights reserved.
 //
 
 #import "NSDictionary+Helpers.h"
 
 @implementation NSDictionary (Helpers)
 
-- (NSURL*)urlForKey:(id)key
+- (id)objectAtIndex:(NSUInteger)index
 {
-    NSString *str = [self objectForKey:key];
-    if (![str isKindOfClass:[NSString class]] || [str isEqualToString:@""]) return nil;
-    return [NSURL URLWithString:str];
+    return [self objectForKey:[NSNumber numberWithUnsignedInteger:index]];
 }
 
-- (id)objectNotNullForKey:(id)key
+- (NSInteger)integerForKey:(id)key
 {
-    id obj = [self objectForKey:key];
-    return [obj isKindOfClass:[NSNull class]] ? nil : obj;
+    return [self integerForKey:key default:NSNotFound];
 }
 
-- (NSDate *)dateForKey:(id)key {
-    NSString *str = [self objectForKey:key];
-    if (![str isKindOfClass:[NSString class]]) return nil;
-
-    NSDateFormatter *dateFormatter = [NSDateFormatter new];
-    [dateFormatter setDateFormat:@"yyyy-MM-dd"];
-    
-    return [dateFormatter dateFromString:[self objectForKey:key]];
+- (NSInteger)integerForKey:(id)key default:(NSInteger)def
+{
+    id obj = [self nonNullObjectForKey:key];
+    return [obj respondsToSelector:@selector(integerValue)]? [obj integerValue] : def;
 }
 
-- (NSNumber *)boolForKey:(id)key {
-    NSString *str = [self objectForKey:key];
-    if (![str isKindOfClass:[NSString class]]) return nil;
-    
-    return [NSNumber numberWithInt:[str boolValue]];
+- (NSUInteger)unsignedIntegerForKey:(id)key
+{
+    return [self unsignedIntegerForKey:key default:NSNotFound];
 }
 
-- (NSNumber *)intForKey:(id)key {
-    NSString *str = [self objectForKey:key];
-    if (![str isKindOfClass:[NSString class]]) return nil;
-    
-    return [NSNumber numberWithInt:[str intValue]];
+- (NSUInteger)unsignedIntegerForKey:(id)key default:(NSUInteger)def
+{
+    id obj = [self nonNullObjectForKey:key];
+    if (![obj respondsToSelector:@selector(integerValue)]) return def;
+    return [obj respondsToSelector:@selector(unsignedIntegerValue)] ? [obj unsignedIntegerValue] : (NSUInteger) [obj integerValue];
 }
 
-- (NSNumber *)floatForKey:(id)key {
-    NSString *str = [self objectForKey:key];
-    if (![str isKindOfClass:[NSString class]]) return nil;
-    
-    return [NSNumber numberWithInt:[str floatValue]];
+- (BOOL)boolForKey:(id)key
+{
+    return [[self numberForKey:key] boolValue];
+}
+
+- (float)floatForKey:(id)key
+{
+    return [[self numberForKey:key] floatValue];
+}
+
+- (double)doubleForKey:(id)key
+{
+    return [[self numberForKey:key] doubleValue];
+}
+
+- (id)nonNullObjectForKey:(id)key
+{
+    id value = [self objectForKey:key];
+    return [value isKindOfClass:[NSNull class]] ? nil : value;
+}
+
+- (NSDate*)dateForKey:(id)key formatter:(NSDateFormatter*)formatter
+{
+    id date = [self nonNullObjectForKey:key];
+    return [date isKindOfClass:[NSDate class]] ? date : [formatter dateFromString:[date description]];
+}
+
+- (NSString*)stringForKey:(id)key
+{
+    id obj = [self nonNullObjectForKey:key];
+    return [obj isKindOfClass:[NSString class]] ? obj : nil;
+}
+
+- (NSDictionary*)dictionaryForKey:(id)key
+{
+    id obj = [self nonNullObjectForKey:key];
+    return [obj isKindOfClass:[NSDictionary class]] ? obj : nil;
+}
+
+- (NSArray*)arrayForKey:(id)key
+{
+    id obj = [self nonNullObjectForKey:key];
+    return [obj isKindOfClass:[NSArray class]] ? obj : nil;
+}
+
+- (NSNumber*)numberForKey:(id)key
+{
+    id obj = [self nonNullObjectForKey:key];
+    return [obj isKindOfClass:[NSNumber class]] ? obj : nil;
+}
+
+@end
+
+@implementation NSMutableDictionary (Helpers)
+
+- (void)replaceObjectAtIndex:(NSUInteger)index withObject:(id)obj
+{
+    [self setObject:obj forKey:[NSNumber numberWithUnsignedInteger:index]];
 }
 
 @end
